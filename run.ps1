0d88012fe514eb91bd8a6c7ba2ca01b26e86556f
ellenjane001 <ellenjane.000001998@gmail.com>
2022-09-23 19:42:15 +0800

    added template for exercises 11 and 12 -- 09/23/2022 19:42:14

---
+++ run.ps1
@@ -0,0 +1,7 @@
+# git init
+# git branch -M main
+# git remote add origin https://github.com/ellenjane001/combinators.git
+git add .
+$commit = Read-Host -Prompt 'commit message'
+git commit -m "$commit -- $(Get-Date)"
+# git push
