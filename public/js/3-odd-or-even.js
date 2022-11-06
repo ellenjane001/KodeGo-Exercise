87a26b03e7f3573b79407f5df09600ef50392b28
ellenjane001 <ellenjane.000001998@gmail.com>
2022-11-06 09:22:07 +0800

    added odd or even -- 11/06/2022 09:22:07

---
+++ public/js/3-odd-or-even.js
@@ -0,0 +1,30 @@
+const odd_or_even = () => {
+    let val = document.getElementById('number')
+    if (val.value !== '') {
+        let valNum = parseInt(val.value)
+        if (valNum % 2 === 0) {
+            alert(`${valNum} is an EVEN Number`, 'success')
+            val.value = ''
+        }
+        else if (valNum % 2 !== 0) {
+            alert(`${valNum} is an ODD Number`, 'warning')
+            val.value = ''
+        }
+    } else
+        val.style.border = 'red 1px solid'
+}
+document.getElementById('verifier').onclick = odd_or_even
+const alertPlaceholder = document.getElementById('result')
+const alert = (message, type) => {
+    const wrapper = document.createElement('div')
+    wrapper.innerHTML = [
+        `<div class="alert alert-${type} alert-dismissible" role="alert">`,
+        `   <div>${message}</div>`,
+        '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+        '</div>'
+    ].join('')
+    alertPlaceholder.append(wrapper)
+    setTimeout(function () {
+        alertPlaceholder.removeChild(alertPlaceholder.firstChild)
+    }, 5000);
+}
