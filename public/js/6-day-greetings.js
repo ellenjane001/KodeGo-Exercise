77eb49e17938858c135fa0d41756182e37542b26
ellenjane001 <ellenjane.000001998@gmail.com>
2022-11-09 17:01:47 +0800

    fixed display and function

--- public/js/6-day-greetings.js
+++ public/js/6-day-greetings.js
@@ -13,28 +13,37 @@ document.getElementById('greet').onclick = () => {
             case week[1]:
                 g = `Have a marvelous <strong>${week[1]}</strong> and a wonderful week!`
                 /* quotes from (https://allwording.com/have-a-great-monday/#:~:text=Hope%20your%20week%20gets%20off,most%20of%20the%20week%20ahead!)*/
+                date.value = ''
                 break
             case week[2]:
                 g = `<strong>${week[2]}</strong> merely means that you made it through Monday, which is an accomplishment. To you, have a happy <strong>${week[2]}</strong>.`
                 /* quotes from (https://www.143greetings.com/tuesday-messages.html)*/
+                date.value = ''
                 break
             case week[3]:
                 g = `Happy <strong>${week[3]}</strong>! Cheer up! there are still two days left until the weekend.`
                 /* quotes from (https://www.wishesmsg.com/wednesday-wishes-messages/)*/
+                date.value = ''
                 break
             case week[4]:
                 g = `Focus on what you want in life rather than what you have achieved. Happy ${week[4]}!`
                 /* quotes from (https://www.wedskenya.com/thursday-wishes-messages/)*/
+                date.value = ''
                 break
             case week[5]:
                 g = `Good morning and happy ${week[5]}. Expect greatness today.`
                 /* quotes from (https://poemore.com/daily-messages/friday-wishes-messages/)*/
+                date.value = ''
                 break
             case week[6]:
                 g = `Wish you have a blissful weekend. Happy ${week[6]}!`
+                /* quotes from https://www.wishesmsg.com/saturday-wishes-messages/ */
+                date.value = ''
                 break
             default:
                 g = 'Invalid day'
+                date.value = ''
+                break
         }
         greetings.innerHTML = g
     }
